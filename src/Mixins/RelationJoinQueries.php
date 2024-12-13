b1c8bf214a8d887d0f15b0acefe2cf139dc36b5b
Tyler <tylernathanreed@gmail.com>
2024-12-13 14:52:08 -0600

    + Added support for new relation generics

--- src/Mixins/RelationJoinQueries.php
+++ src/Mixins/RelationJoinQueries.php
@@ -4,10 +4,17 @@ namespace Reedware\LaravelRelationJoins\Mixins;
 
 use Closure;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Reedware\LaravelRelationJoins\RelationJoinQuery;
 
-/** @mixin Relation */
+/**
+ * @template TRelatedModel of Model
+ * @template TDeclaringModel of Model
+ * @template TResult
+ *
+ * @mixin Relation<TRelatedModel,TDeclaringModel,TResult>
+ */
 class RelationJoinQueries
 {
     /**
@@ -17,9 +24,13 @@ class RelationJoinQueries
     {
         /**
          * Adds the constraints for a relationship join.
+         *
+         * @param  Builder<TRelatedModel>  $query
+         * @param  Builder<TDeclaringModel>  $parentQuery
+         * @return Builder<TRelatedModel>
          */
         return function (Builder $query, Builder $parentQuery, string $type = 'inner', ?string $alias = null): Builder {
-            /** @var Relation $this */
+            /** @var Relation<TRelatedModel,TDeclaringModel,TResult> $this */
             return RelationJoinQuery::get($this, $query, $parentQuery, $type, $alias);
         };
     }
